9caa22762c745d36593a004d2b01a6d524899191
Ravadakusuma <150145225+Ravadakusuma@users.noreply.github.com>
2024-03-12 12:47:30 +0530

    Add files via upload

---
+++ hiiiiiiiii.py
@@ -0,0 +1,16 @@
+"""
+Your module description
+"""
+text1="malwmrllpllallalwgpdpafv"
+print(len(text1))
+
+text2="nqhlcgshlvealylvcgergffytpktrr"
+print(len(text2))
+text3="eaedlqvgqvelgggpgagslqplavlegslqkrg"
+print(len(text3))
+
+text4="iveqcctsicslyqlenycn "
+print(len(text4))
+
+
+
